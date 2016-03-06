9254eb4857c2d56022d7c5bba36f1e95e258ae1b
Flávio Veloso <flaviovs@magnux.com>
2016-03-06 11:58:04 -0300

    Remove unused hidden field with requester address

--- view/confirm.php
+++ view/confirm.php
@@ -18,7 +18,6 @@
 
 <form method="POST" action="<?php echo $action ?>">
   <input type="hidden" name="form_token" value="<?php echo htmlspecialchars($form_token) ?>">
-  <input type="hidden" name="req_email" value="<?php echo htmlspecialchars($req_email) ?>">
   <input type="hidden" name="user_email" value="<?php echo htmlspecialchars($user_email) ?>">
   <input type="hidden" name="instructions" value="<?php echo $instructions ?>">
 
